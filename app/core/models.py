fcff8a600c514bbb15385bda25b200430d90e621
Guilherme Castello <guilherme.castello.clear@gmail.com>
2023-12-12 16:18:05 -0300

    Feat: Creating and testing user and improved tests for user

--- app/core/models.py
+++ app/core/models.py
@@ -3,8 +3,18 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.model(email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self.db)
 
         return user
