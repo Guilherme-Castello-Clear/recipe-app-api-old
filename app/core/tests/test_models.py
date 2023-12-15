7fdab850197d8d70b829f8388c2c088464482b08
Guilherme Castello <guilherme.castello.clear@gmail.com>
2023-12-15 15:04:37 -0300

    Feat: Created tag model, listing tag and added tests for it

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,10 @@
+from core import models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+def sample_user(email='test@castello.com', password='testpass'):
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
@@ -31,3 +36,11 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
